79fe80706ea5973817fd6f38acb9b581876d7852
xinghengwang <xing@moesif.com>
2022-09-13 15:59:18 -0700

    adding an example for jwt express

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var superagent = require('superagent');
-
-// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
-var httpProxy = require('http-proxy');
 
 var moesif = require('moesif-nodejs');
 
+var JsonWebToken = require('jsonwebtoken');
+var { expressjwt: jwt } = require("express-jwt");
+
 var port = process.env.PORT || 7500;
+
+const FAKE_JWT_SECRETE_KEY = 'keep-your-secret-safe';
 
 // Set the options, the only required field is applicationId.
 var moesifOptions = {
@@ -46,9 +47,6 @@ var moesifOptions = {
     }
   },
 
-
-  baseUri: 'https://api-dev.moesif.net',
-
   // batchMaxTime: 10000,
   // batchSize: 15,
   disableBatching: true,
@@ -68,94 +66,49 @@ var moesifOptions = {
 var moesifMiddleware = moesif(moesifOptions);
 
 app.use(moesifMiddleware);
-moesifMiddleware.startCaptureOutgoing();
-
-app.get('/', function (req, res) {
-  console.log(req.body);
-  res.send('hello world!');
-});
-
-app.post('/multipart', function (req, res) {
-  console.log('inside multi part');
-  console.log(req.body);
-  res.send('received');
-});
 
 
-app.get('/large-string-response', function(req, res) {
-  var really_long_string = (new Array(10001)).join("x");
-  res.send(really_long_string);
-});
+var jwtRoutes = express.Router();
 
-// router are prefaced by /api
-// and uses bodyParser
-var router = express.Router();
+// this is just an example for generate a JWT token that can be used for testing
+// express jwt middle.
+// this route is NOT protected because it is a fake example.
+// you should have a real process for
+// safely distributing JWT token
+jwtRoutes.get('/generate-jwt-token', function(req, res) {
+  const jwtData = {
+    foo: 'bar'
+  };
+  const userId = 'firstuser';
 
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(express.json({limit: '50mb', extended: true}));
-router.use(bodyParser.text({type: 'text/plain'}))
-
-router.get('/', function(req, res) {
-  console.log('req body in customer api');
-  console.log(req.body);
-  res.json({ message: 'first json api'});
-});
-
-router.post('/large', function(req, res) {
-  console.log('req body in customer api');
-  console.log(req.body);
-  res.json({ message: 'post successful'})
-});
-
-router.get('/large-object-response', function(req, res) {
-  var reallyBigArray = (new Array(10001)).fill('hi');
-  res.json(reallyBigArray);
-});
-
-router.get('/outgoing/posts', function(req, res) {
-  console.log('outgoing is called');
-  superagent.get('https://jsonplaceholder.typicode.com/todos/2').then(function (response) {
-    console.log('back from outoging');
-    console.log(response.body);
-    res.json({ fromTypicode: response.body });
-  }).catch(function(err) {
-    res.status(500).json(err);
+  const token = JsonWebToken.sign(jwtData, FAKE_JWT_SECRETE_KEY, {
+    expiresIn: '48h',
+    subject: userId,
+    issuer: `https://acmeinc.com/`,
+    audience: 'world',
+    algorithm: 'HS256'
   });
+
+  res.json({ token });
 });
 
-/**
- * Example using http-proxy.
- */
 
-var proxyRoute = express.Router();
-const proxy = httpProxy.createProxyServer();
+// a fake route protected by express jwt middleware.
+jwtRoutes.get(
+  "/protected-by-jwt-check",
+  jwt({ secret: FAKE_JWT_SECRETE_KEY, algorithms: ["HS256"] }),
+  function (req, res) {
+    console.log('decoded JWT token');
+    console.log(JSON.stringify(req.auth, null, '  '));
 
-proxy.on('error', (error, req, res) => {
-  if (error.code !== 'ECONNRESET') {
-    console.error('proxy error', error);
+    // again, express jwt decodes the token (assume valid) into req.auth
+    res.json({
+      decoded_token: req.auth
+    });
   }
-  if (!res.headersSent) {
-    res.writeHead(500, { 'content-type': 'application/json' });
-  }
+);
 
-  const json = { error: 'proxy_error', reason: error.message };
-  res.end(JSON.stringify(json));
-});
-
-
-proxyRoute.use('/', (req, res) => {
-  proxy.web(req, res, {
-    target: 'http://jsonplaceholder.typicode.com',
-    ws: false,
-    changeOrigin: true,
-  });
-});
-
-
-app.use('/api', router);
-
-app.use('/proxy', proxyRoute);
-
+app.use('/jwt-test', jwtRoutes);
 
 app.listen(port, function() {
   console.log('Example app is listening on port ' + port);
