aa5143d4ee9dc6fe98585cdf2f3ac15c0b2e9b57
xinghengwang <xing@moesif.com>
2025-12-02 10:23:00 -0800

    to single route

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var port = process.env.PORT || 5050
 // Set the options, the only required field is applicationId.
 var moesifOptions = {
 
-  applicationId: process.env.MOESIF_APPLICATION_ID || 'Your Moesif Application Id',
+  applicationId: process.env.MOESIF_APPLICATION_ID || 'YOUR_MOESIF_APPLICATION_ID',
 
   baseUri: 'https://api.moesif.net',
 
@@ -43,18 +43,6 @@ var moesifOptions = {
     return undefined;
   },
 
-  getSessionToken: function (req, res) {
-    return req.headers['Authorization'];
-  },
-
-  getMetadata: function (req, res) {
-    return {
-      foo: 'express',
-      bar: 'example',
-      my_date_field: (new Date()).toISOString()
-    }
-  },
-
   // batchMaxTime: 10000,
   // batchSize: 15,
   disableBatching: true,
@@ -70,141 +58,18 @@ var moesifOptions = {
   }
 };
 
-moesifOptions.maskContent = function (event) {
-  console.log('event before masking' + JSON.stringify(event));
-  const newEvent = _.omit(event, ['request.headers.authorization']);
-  console.log('event after masking' + JSON.stringify(newEvent));
-  return newEvent;
-}
 
 var moesifMiddleware = moesif(moesifOptions);
 
 app.use(moesifMiddleware);
-// moesifMiddleware.startCaptureOutgoing();
-
-app.get('/', function (req, res) {
-  console.log(req.body);
-  res.send('hello world!');
-});
-
-app.post('/multipart', function (req, res) {
-  console.log('inside multi part');
-  console.log(req.body);
-  res.send('received');
-});
-
-app.get('/large-string-response', function(req, res) {
-  var really_long_string = (new Array(10001)).join("x");
-  res.send(really_long_string);
-});
-
-// router are prefaced by /api
-// and uses bodyParser
-var router = express.Router();
-
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(express.json({limit: '50mb', extended: true}));
-router.use(bodyParser.text({type: 'text/plain'}))
-
-router.get('/', function(req, res) {
-  console.log('req body in customer api');
-  console.log(req.body);
-  res.json({ message: 'first json api'});
-});
-
-router.post('/large', function(req, res) {
-  console.log('req body in customer api');
-  console.log(req.body);
-  res.json({ message: 'post successful'})
-});
-
-router.get('/large-object-response', function(req, res) {
-  var reallyBigArray = (new Array(10001)).fill('hi');
-  res.json(reallyBigArray);
-});
-
-router.get('/outgoing/posts', function(req, res) {
-  console.log('outgoing is called');
-  superagent.get('https://jsonplaceholder.typicode.com/todos/2').then(function (response) {
-    console.log('back from outoging');
-    console.log(response.body);
-    res.json({ fromTypicode: response.body });
-  }).catch(function(err) {
-    res.status(500).json(err);
-  });
-});
-
-/**
- * Example using http-proxy.
- */
-
-var proxyRoute = express.Router();
-const proxy = httpProxy.createProxyServer();
-
-proxy.on('error', (error, req, res) => {
-  if (error.code !== 'ECONNRESET') {
-    console.error('proxy error', error);
-  }
-  if (!res.headersSent) {
-    res.writeHead(500, { 'content-type': 'application/json' });
-  }
-
-  const json = { error: 'proxy_error', reason: error.message };
-  res.end(JSON.stringify(json));
-});
-
-
-proxyRoute.use('/', (req, res) => {
-  proxy.web(req, res, {
-    target: 'http://jsonplaceholder.typicode.com',
-    ws: false,
-    changeOrigin: true,
-  });
-});
 
 var governanceRoutes = express.Router();
 
-governanceRoutes.get('/no_italy', (req, res) => {
+governanceRoutes.get('/api/recons/11af661e-4445-4d30-aaae-c651fa70474c/initiate', (req, res) => {
   res.status(200).send({
     success: true
   });
 });
-
-governanceRoutes.get('/company1', (req, res) => {
-  res.status(200).send({
-    success: true
-  });
-});
-
-
-governanceRoutes.get('/canada', (req, res) => {
-  res.status(200).send({
-    success: true
-  });
-});
-
-governanceRoutes.get('/cairo', (req, res) => {
-  res.status(200).send({
-    success: true
-  });
-});
-
-governanceRoutes.get('/for_companies_in_japan_only', (req, res) => {
-  res.status(200).send({
-    success: true
-  });
-});
-
-governanceRoutes.get('/random', (req, res) => {
-  res.status(200).send({
-    success: true
-  });
-});
-
-
-app.use('/api', router);
-
-app.use('/proxy', proxyRoute);
 
 app.use('/gov', governanceRoutes);
 
